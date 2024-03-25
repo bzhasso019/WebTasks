edf806604ddb83575c602edba3b28d6df3abc97c
bzhasso019 <144123070+bzhasso019@users.noreply.github.com>
2024-03-25 18:58:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Регистрационная форма</title>
+    <style>
+        /* Стили для вашей формы */
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Введите данные</h2>
+        <form action="save_form.php" method="post">
+            <!-- Поля вашей формы -->
+            <div class="button-container">
+                <button type="submit">Сохранить</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>

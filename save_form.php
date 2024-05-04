7bdda94b52ef0eb7ec9bf9d737e7775aa6ddedfa
bzhasso019 <144123070+bzhasso019@users.noreply.github.com>
2024-05-04 23:48:15 +0300

    Update save_form.php

--- save_form.php
+++ save_form.php
@@ -53,7 +53,7 @@ if (!empty($errors)) {
 }
 
 // Создание записи в таблице с формой
-$stmt = $conn->prepare("INSERT INTO forms (fio, phone, email, dob, gender, bio, contract) VALUES (?, ?, ?, ?, ?, ?, ?)");
+$stmt = $conn->prepare("INSERT INTO form_data (fio, phone, email, dob, gender, bio, contract) VALUES (?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("sssssss", $fio, $phone, $email, $dob, $gender, $bio, $contract);
 $stmt->execute();
 $stmt->close();
